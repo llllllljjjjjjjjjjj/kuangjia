ac88c78c6e5a3b943bc020a5456cec5c3a09407c
ljj <1357023255@qq.com>
2026-05-22 22:10:37 +0800

    加了一些过滤的内置对象，防止代理后this指向问题

--- 框架_min_min/config/config.js
+++ 框架_min_min/config/config.js
@@ -1,15 +1,20 @@
 //全局对象配置
-debugger 
+debugger
 ldvm = {
-    "toolsFunc":{},//功能函数相关，插件
-    "envFunc":{},//具体环境实现相关
+    "toolsFunc": {},//功能函数相关，插件
+    "envFunc": {},//具体环境实现相关
     "config": {}, //配置相关
     "memory": {}, //内存相关
 }
 ldvm.config.proxy = true//是否开启代理
 ldvm.config.print = true//是否输出日志
 ldvm.memory.symbolProxy = Symbol("proxy")
-ldvm.memory.filterProxyProp = [ldvm.memory.symbolProxy,Symbol.toPrimitive,  "eval", ]//需要过滤的属性
+ldvm.memory.filterProxyProp = [
+    ldvm.memory.symbolProxy, 
+    Symbol.toPrimitive, "eval", Object.prototype, Array.prototype, Function.prototype,
+    String.prototype, Number.prototype, Boolean.prototype,
+    Math, Date, RegExp, JSON, Promise
+]//需要过滤的属性
 ldvm.memory.symbolData = Symbol("data"); // 保存当前对象上原型的属性
 ldvm.memory.tag = []//存储tag标签
 
